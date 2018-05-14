3d4bbd393c1dd11b98ff545309bae46a80254007
shen11961677@163.com <shenliwei521>
2018-05-14 22:56:23 +0800

    提交服务之间鉴权

---
+++ skycloud-manager/src/main/java/com/skycloud/manager/AdminApplication.java
@@ -0,0 +1,33 @@
+package com.skycloud.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author sky
+ * @description
+ * @create 2018-05-08 下午8:38
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class AdminApplication {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    /**
+     * 入口
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
