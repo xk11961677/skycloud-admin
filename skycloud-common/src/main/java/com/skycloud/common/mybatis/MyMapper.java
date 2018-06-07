9c4cc47f49cad1c168605f53148a53a5a68d5332
sky <shen11961677@163.com>
2018-06-07 16:36:17 +0800

    代码生成器

---
+++ skycloud-common/src/main/java/com/skycloud/common/mybatis/MyMapper.java
@@ -0,0 +1,12 @@
+package com.skycloud.common.mybatis;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+
+/**
+ * The interface My mapper.
+ *
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+}
