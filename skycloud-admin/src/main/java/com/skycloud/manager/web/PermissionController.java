37ff5edd101a4734d340f4b0e84707a4ed3d0d93
sky <shen11961677@163.com>
2018-06-07 16:30:24 +0800

    代码生成器

---
+++ skycloud-admin/src/main/java/com/skycloud/manager/web/PermissionController.java
@@ -0,0 +1,15 @@
+package com.skycloud.manager.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sky
+ * @description
+ * @create 2018-05-08 下午9:10
+ **/
+@RestController
+@RequestMapping("permission")
+public class PermissionController {
+
+}
